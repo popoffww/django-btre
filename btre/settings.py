508749a1f789714a716f33baccd4c00e2f5edcc5
popoffww <popoffww@yandex.ru>
2020-10-14 14:44:46 +0600

    Apps, URLs, Templates & Static

--- btre/settings.py
+++ btre/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'pages',
+    'listings',
+    'realtors',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'btre/static',
+]
---
+++ listings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='listings'),
+    path('<int:listing_id>', listing, name='listing'),
+    path('search', index, name='search'),
+]
